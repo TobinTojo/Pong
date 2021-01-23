e2fa2e0e31f918456aa0e41438ca13bbbdce9a5d
TobinTojo <69814148+TobinTojo@users.noreply.github.com>
2021-01-23 14:15:42 -0500

    Add files via upload

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picLeftPaddle = new System.Windows.Forms.PictureBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.picRightPaddle = new System.Windows.Forms.PictureBox();
+            this.tmrGame = new System.Windows.Forms.Timer(this.components);
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picLeftPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRightPaddle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLeftPaddle
+            // 
+            this.picLeftPaddle.BackColor = System.Drawing.Color.White;
+            this.picLeftPaddle.Location = new System.Drawing.Point(20, 280);
+            this.picLeftPaddle.Name = "picLeftPaddle";
+            this.picLeftPaddle.Size = new System.Drawing.Size(20, 150);
+            this.picLeftPaddle.TabIndex = 0;
+            this.picLeftPaddle.TabStop = false;
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.White;
+            this.picBall.Location = new System.Drawing.Point(490, 340);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(20, 20);
+            this.picBall.TabIndex = 2;
+            this.picBall.TabStop = false;
+            // 
+            // picRightPaddle
+            // 
+            this.picRightPaddle.BackColor = System.Drawing.Color.White;
+            this.picRightPaddle.Location = new System.Drawing.Point(964, 280);
+            this.picRightPaddle.Name = "picRightPaddle";
+            this.picRightPaddle.Size = new System.Drawing.Size(20, 150);
+            this.picRightPaddle.TabIndex = 3;
+            this.picRightPaddle.TabStop = false;
+            // 
+            // tmrGame
+            // 
+            this.tmrGame.Interval = 10;
+            this.tmrGame.Tick += new System.EventHandler(this.tmrGame_Tick);
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.ForeColor = System.Drawing.Color.White;
+            this.lblStart.Location = new System.Drawing.Point(327, 375);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(373, 39);
+            this.lblStart.TabIndex = 4;
+            this.lblStart.Text = "Press ENTER to Begin!";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer2.Location = new System.Drawing.Point(810, 75);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(36, 39);
+            this.lblPlayer2.TabIndex = 5;
+            this.lblPlayer2.Text = "0";
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer1.Location = new System.Drawing.Point(206, 75);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(36, 39);
+            this.lblPlayer1.TabIndex = 6;
+            this.lblPlayer1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1006, 753);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.picRightPaddle);
+            this.Controls.Add(this.picLeftPaddle);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picLeftPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picRightPaddle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLeftPaddle;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.PictureBox picRightPaddle;
+        private System.Windows.Forms.Timer tmrGame;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.Label lblPlayer2;
+        private System.Windows.Forms.Label lblPlayer1;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+
+namespace Pong
+{
+    enum PaddleState
+    {
+        None,
+        Up,
+        Down
+    }
+
+
+    public partial class Form1 : Form
+    {
+        //global variables
+        PaddleState LeftPaddle = PaddleState.None;
+        PaddleState RightPaddle = PaddleState.None;
+        Vector2 Velocity;
+        Vector2 Position;
+        float Speed;
+        int Player1 = 0, Player2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //move left paddle
+            if (e.KeyCode == Keys.W)
+            {
+                LeftPaddle = PaddleState.Up;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                LeftPaddle = PaddleState.Down;
+            }
+            //move right paddle
+            if (e.KeyCode == Keys.Up)
+            {
+                RightPaddle = PaddleState.Up;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                RightPaddle = PaddleState.Down;
+            }
+            if (e.KeyCode == Keys.Return)
+            {
+                //reset
+                Reset();
+
+                lblStart.Visible = false;
+                tmrGame.Enabled = true;
+            }
+
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //PaddleState is None for all
+            if (e.KeyCode == Keys.W)
+            {
+                LeftPaddle = PaddleState.None;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                LeftPaddle = PaddleState.None;
+            }
+            
+            if (e.KeyCode == Keys.Up)
+            {
+                RightPaddle = PaddleState.None;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                RightPaddle = PaddleState.None;
+            }
+
+        }
+
+        private void tmrGame_Tick(object sender, EventArgs e)
+        {
+            //detect if ball hits a paddle
+            if (picLeftPaddle.Bounds.IntersectsWith(picBall.Bounds) ||
+                picRightPaddle.Bounds.IntersectsWith(picBall.Bounds))
+            {
+                //we have a collision
+                //set max speed
+                if (Speed < 11)
+                {
+                    Velocity.X = -1.1f * Velocity.X;
+                    Velocity.Y = 1.1f * Velocity.Y;
+                    Speed = (float)Velocity.Length();
+                }
+                else
+                {
+                    Velocity.X = -1 * Velocity.X;
+                }
+
+            }
+
+
+            //detect if the ball hits top or bottom wall
+            if (picBall.Top <= 0 || picBall.Bottom >= ClientSize.Height)
+            {
+                Velocity.Y = -1 * Velocity.Y;
+            }
+
+            //check if ball scores
+            if (picBall.Left <= 0)
+            {
+                Player2++;
+                lblPlayer2.Text = Player2.ToString();
+                Reset();
+            }
+            else if (picBall.Right >= ClientSize.Width)
+            {
+                Player1++;
+                lblPlayer1.Text = Player1.ToString();
+                Reset();
+            }
+
+
+            //move the ball
+            Position = Position + Velocity;
+            picBall.Left = (int)Position.X;
+            picBall.Top = (int)Position.Y;
+
+            //move the paddles
+            if (LeftPaddle == PaddleState.Up)
+            {
+                picLeftPaddle.Top -= 5;
+            }
+            if (LeftPaddle == PaddleState.Down)
+            {
+                picLeftPaddle.Top += 5;
+            }
+            if (RightPaddle == PaddleState.Up)
+            {
+                picRightPaddle.Top -= 5;
+            }
+            if (RightPaddle == PaddleState.Down)
+            {
+                picRightPaddle.Top += 5;
+            }
+
+
+        }
+
+
+        private float FindRandomAngle()
+        {
+            //generate a random angle for the ball
+            Random RandomAngle = new Random();
+
+            int R = 0;
+            while (R % 2 == 0)
+            {
+                R = RandomAngle.Next(1, 8);
+            }
+            //convert angle to radian from degrees
+            float Angle = MathHelper.ToRadians(R * 45);
+
+            return Angle;
+        }
+
+        private void Reset()
+        {
+            //set the ball in middle of screen
+            float Width = (ClientSize.Width + picBall.Width) / 2;
+            float Height = (ClientSize.Height + picBall.Height) / 2;
+            Position = new Vector2(Width, Height);
+            //set velocity of ball
+            float Angle = FindRandomAngle();
+            Speed = 6;
+            float Vx = Speed * (float)Math.Cos(Angle);
+            float Vy = Speed * (float)Math.Sin(Angle);
+            Velocity = new Vector2(Vx, Vy);
+
+        }
+
+
+    }
+}
